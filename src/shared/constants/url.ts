03e7ad4cf807b374310beabb2a8995fb4e6156eb
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-10-21 18:06:10 +0900

    feat: router url 관련 내용 상수화 (url.ts 파일 생성 및 작성)

---
+++ src/shared/constants/url.ts
@@ -0,0 +1,10 @@
+export const ROUTER_URL = {
+  HOME: '/',
+  ABOUT: '/about',
+  PROJECT: '/project',
+  APPLY: '/apply',
+  ACTIVITY: '/activity',
+  WEBFOUNDERS: '/webfounders',
+
+  // 추가되는 거 있음 여기에 작성
+};
