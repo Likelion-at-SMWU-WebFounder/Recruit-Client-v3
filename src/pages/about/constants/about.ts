109aeff4002fb36bec55eebdaa3434af5ba3bc2c
ynzung <syj91105@sookmyung.ac.kr>
2025-10-21 19:32:02 +0900

    chore: about 페이지에서 사용되는 title, subtitle 문구 상수화(#13)

---
+++ src/pages/about/constants/about.ts
@@ -0,0 +1,17 @@
+// about 페이지에서 사용되는 title, subTitle 등의 상수 모음 파일
+
+export const TITLE = {
+  TITLE_1: '아이디어를 서비스로, 캠퍼스에서 세상으로',
+  DESCRIPTION_1: '숙명이 부르면, 멋대로 답한다',
+  TITLE_2: '함께여서 더 대담한 숙명',
+  DESCRIPTION_2: '개발이 낯선 비전공자도, 깊이를 찾는 전공자도\n함께라면 한계를 넘어설 수 있다고 믿습니다.',
+};
+
+export const SUB_TITLE = {
+  CORE_VALUE: 'core value',
+  CORE_VALUE_DESCRIPTION: '우리가 믿는 가치',
+  OUR_PEOPLE: 'our people',
+  OUR_PEOPLE_DESCRIPTION: '함께하고 싶은 사람',
+  JOIN_US: 'join us',
+  JOIN_US_DESCRIPTION: '지원하기',
+};
