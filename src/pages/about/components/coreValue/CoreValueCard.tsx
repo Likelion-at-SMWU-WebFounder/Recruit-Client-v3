1f5047b9f1dbd5d8a6ea71c29829aeeb042c5962
ynzung <syj91105@sookmyung.ac.kr>
2025-10-23 17:35:06 +0900

    style: 패딩 미세 조정 및 이미지 사이즈•비율 수정, md 브레이크포인트 정리(#13)

--- src/pages/about/components/coreValue/CoreValueCard.tsx
+++ src/pages/about/components/coreValue/CoreValueCard.tsx
@@ -30,9 +30,9 @@ const CoreValueCard = ({ keyword, description, cardImage, highlightedWord }: Cor
   };
 
   // 카드 패딩 스타일 설정
-  const paddingClasses = 'px-[1.75rem] py-[2.37rem] md:px-[1.75rem] md:py-[2.37rem] lg:px-[3.12rem] lg:py-[4.37rem]';
+  const paddingClasses = 'px-[1.75rem] py-[2.38rem] lg:px-[3.12rem] lg:py-[4.37rem]';
   // 카드 이미지 크기 스타일 설정
-  const imageSizeClasses = 'w-[14.375rem] h-[16.5rem] md:w-[18rem] md:h-[20.25rem] lg:w-[32.3125rem] lg:h-[36.375rem]';
+  const imageSizeClasses = 'w-[18rem] h-[20.25rem] aspect-[8/9] lg:w-[32.3125rem] lg:h-[36.375rem] lg:aspect-[517/582]';
   // 카드 키워드 크기 스타일 설정
   const keywordSizeClasses = 'text-[1.5rem] font-[700] lg:font-[600] md:text-[1.5rem] lg:text-[2.625rem]';
   // 카드 설명 크기 스타일 설정
