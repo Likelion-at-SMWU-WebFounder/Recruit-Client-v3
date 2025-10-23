427f06b25131c4611e973a6e4467ecbb665cd90d
ynzung <syj91105@sookmyung.ac.kr>
2025-10-23 18:18:09 +0900

    feat: about 페이지 파일 구현(#13)

---
+++ src/pages/about/About.tsx
@@ -0,0 +1,11 @@
+import CoreValuesSection from './components/coreValue/CoreValuesSection';
+
+const About = () => {
+  return (
+    <div>
+      <CoreValuesSection />
+    </div>
+  );
+};
+
+export default About;
