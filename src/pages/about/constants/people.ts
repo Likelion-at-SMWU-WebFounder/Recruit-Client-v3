1d96bae792b1a8dc3be5fb3de54fce721879af0b
ynzung <syj91105@sookmyung.ac.kr>
2025-11-18 15:45:54 +0900

    chore: about 페이지에서 사용되는 our people 카드 데이터 상수화(#20)

---
+++ src/pages/about/constants/people.ts
@@ -0,0 +1,43 @@
+import arrowIcon from '@pages/about/assets/people/arrow.svg';
+import flagIcon from '@pages/about/assets/people/flag.svg';
+import lightIcon from '@pages/about/assets/people/light.svg';
+import architectureIcon from '@pages/about/assets/people/architecture.svg';
+import linkIcon from '@pages/about/assets/people/link.svg';
+
+export const PEOPLE_DATA = [
+  {
+    id: 1,
+    icon: arrowIcon,
+    keyword: '달릴 준비가 된 사람',
+    description:
+      '1년 동안 꾸준히 책임감 있게 활동하고,\n매주 화요일 저녁 7시부터 9시까지 참여하며\n여름방학 장기 해커톤에도 함께할 사람을 찾습니다.',
+  },
+  {
+    id: 2,
+    icon: flagIcon,
+    keyword: '끝까지 도전하고 성장하는 사람',
+    description:
+      '포기하지 않고 도전하며,\n모르는 것을 두려워하지 않고\n질문하고 배우는 과정을 즐기는 사람을 환영합니다.',
+  },
+  {
+    id: 3,
+    icon: lightIcon,
+    keyword: '배우고 만들며 꿈꾸는 사람',
+    description:
+      '코딩이 처음이어도 괜찮습니다.\n자신의 아이디어를 웹 서비스로 구현하고\n배포해보고 싶은 열정만 있다면 충분합니다.',
+  },
+  {
+    id: 4,
+    icon: architectureIcon,
+    keyword: '함께하고 싶은 사람',
+    description:
+      '작은 불편에 끊임없이 질문을 던지고,\n내 시선으로 해답을 기획하고 디자인해\n스스로 서비스를 만들고 싶은 사람을 기다립니다.',
+  },
+  {
+    id: 5,
+    icon: linkIcon,
+    keyword: '함께 연결되고 나누는 사람',
+    description:
+      '혼자보다 함께 배우고 성장하며,\n사람들과 네트워크를 만들고 싶은 사람이라면\n언제든 함께할 수 있습니다.',
+  },
+];
