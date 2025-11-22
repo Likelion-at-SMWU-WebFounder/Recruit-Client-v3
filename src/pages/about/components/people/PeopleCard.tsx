07676fe5078fa2af571bbf297f6e38d8da8fd369
ynzung <syj91105@sookmyung.ac.kr>
2025-11-23 01:23:55 +0900

    style: PeopleCard 컴포넌트 스타일 수정(#20)

--- src/pages/about/components/people/PeopleCard.tsx
+++ src/pages/about/components/people/PeopleCard.tsx
@@ -6,12 +6,10 @@ interface PeopleCardProps {
 
 const PeopleCard = ({ icon, keyword, description }: PeopleCardProps) => {
   return (
-    <div className="flex w-[41rem] flex-col items-start gap-[3rem] rounded-[1.25rem] px-[4.75rem] py-[5.88rem] shadow-[0_0_22.7px_0_rgba(27,38,52,0.13)]">
+    <div className="flex h-[35.5rem] w-[40rem] flex-col items-start gap-[3rem] rounded-[1.25rem] px-[4.75rem] py-[5.88rem] shadow-[0_0_22.7px_0_rgba(27,38,52,0.13)]">
       <img src={icon} alt={keyword} className="w-[5rem]" />
-      <div className="text-navyblack text-[2rem] font-[600] md:text-[1.5rem] lg:text-[2.625rem] lg:font-[600]">
-        {keyword}
-      </div>
-      <div className="text-navyblack/70 text-[1.5rem] font-[600] whitespace-pre-line">{description}</div>
+      <div className="text-navyblack text-[2rem] font-[600] md:text-[1.5rem] lg:text-[2rem]">{keyword}</div>
+      <div className="text-navyblack/70 text-[1.5rem] leading-[200%] font-[600] whitespace-pre-line">{description}</div>
     </div>
   );
 };
