82d402e500837517ee8347d89f1df8ac2325f631
ynzung <syj91105@sookmyung.ac.kr>
2025-11-27 19:37:35 +0900

    style: PeopleSection 컴포넌트의 x축 스크롤바 숨김 설정(#20)

--- src/pages/about/components/people/PeopleSection.tsx
+++ src/pages/about/components/people/PeopleSection.tsx
@@ -9,6 +9,8 @@ import { useDesktopScroll } from '@pages/about/hooks/useDesktopScroll';
 import { useMobileScroll } from '@pages/about/hooks/useMobileScroll';
 import useXPositionDrag from '@pages/about/hooks/useXPositionDrag';
 
+import '@pages/about/styles/index.css';
+
 const DESKTOP_CAROUSEL_PADDING_LEFT = 2; // rem
 const DESKTOP_CAROUSEL_TRAILING_SPACE_WIDTH = 17; // rem
 
