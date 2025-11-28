fc4854878dbfe0cb049f37b2073497509eb5d449
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-11-28 16:37:14 +0900

    feat: menu 컴포넌트 항목 구현 (ui 수정 필요) (#17)

--- src/shared/components/Menu.tsx
+++ src/shared/components/Menu.tsx
@@ -1,70 +1,67 @@
-// import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { MENU_ORDER } from '@shared/constants/menu';
+import smwu_lion_logo_dark from '@assets/icons/smwu_lion_logo_dark.svg';
+import smwu_lion_logo_light from '@assets/icons/smwu_lion_logo_white.svg';
+import { KAKAOTALK_URL, INSTAGRAM_URL } from '@shared/constants/url';
+import { BsArrowUpRightCircleFill } from 'react-icons/bs'; // 지원하기 옆 화살표
+import { RiKakaoTalkFill, RiInstagramFill } from 'react-icons/ri';
 
 export interface MenuProps {
-  /* 메뉴 모드 - light / dark */
-  mode?: 'light' | 'dark';
+  menuMode?: 'light' | 'dark';
 }
 
-const Menu = ({ mode = 'light', ...props }: MenuProps) => {
-  // const navigate = useNavigate(); // TODO: 메뉴 네비게이션 구현 시 사용
-
-  /* menu 사용법
-  // 컴포넌트에서 사용
-{MENU_ORDER.map((item) => (
-  <Link key={item.key} to={item.path}>
-    {item.label}
-  </Link>
-))}
-
-// 특정 메뉴 접근
-const homeMenu = MENU_ITEMS.HOME;
-console.log(homeMenu.label); // '홈'
-
-  menu 사용법 끝 ~~~ 
-  ------- */
+const Menu = ({ menuMode = 'light', ...props }: MenuProps) => {
+  // 메뉴 모드에 따른 글, 아이콘 색상 설정
+  const menuModeColor = menuMode === 'light' ? 'navyblack' : 'white';
 
   // 기본 스타일 + 색상 모드
   const baseClasses = [
-    'w-screen inline-flex flex-col justify-center items-center',
-    `${mode === 'light' ? 'bg-white' : 'bg-navyblack  '}`,
+    'w-screen flex flex-row justify-between items-center',
+    `${menuMode === 'light' ? 'text-navyblack' : 'text-white'}`,
   ].join(' ');
 
   // 반응형 padding, gap 스타일 (모바일 기본, md: 태블릿, lg: 데스크톱)
   const responsiveClasses = [
-    'pt-[3.13rem] pb-[1.69rem] gap-[1.25rem]', // mobile
+    'pt-[1rem] pb-[1.69rem] gap-[1.25rem]', // mobile
     'md:pt-[5rem] md:pb-[3.19rem] md:gap-[2.5rem]', // tablet
     'lg:pt-[8.13rem] lg:pb-[3.06rem] lg:gap-[3.06rem]', // desktop
   ].join(' ');
 
   // 반응형 타이틀 사이즈(모바일 기본, md: 태블릿, lg: 데스크톱)
-  const titleClasses = [
-    'font-semibold text-[1rem] md:text-[1.5rem] lg:text-[1.75rem] whitespace-pre-line text-center', //
-    `${mode === 'light' ? 'text-navyblack' : 'text-white  '}`, // 색상 모드
-  ].join(' ');
+  const logoClasses = 'h-[34px], md:h-[42px], lg:h-[60px]';
 
   // 반응형 아이콘 컨테이너 사이즈
   const iconContainerClasses = 'inline-flex flex-row justify-center items-center gap-[2rem]';
-  // TODO: 아이콘 메뉴 구현 시 사용할 스타일들
-  // const iconItemClasses = [
-  //   'inline-flex flex-col justify-center items-center cursor-pointer', // 기본 설정
-  //   'gap-[0.5rem]', // mobile
-  //   'md:gap-[1rem] lg:gap-[1rem]', // tablet, desktop
-  // ].join(' ');
-  // const iconTextClasses = 'font-semibold text-blue text-[0.875rem] md:text-[1rem] lg:text-[1rem]';
-  // const iconSizeClasses = 'text-blue w-[1.5rem] h-[1.5rem] md:w-[1.75rem] md:h-[1.75rem] lg:w-[2rem] lg:h-[2rem]';
-
-  // 반응형 저작권 표시 텍스트 사이즈
-  const copyrightClasses =
-    'text-center text-gray font-normal leading-[150%] text-[0.75rem] md:text-[0.875rem] lg:text-[1rem]';
+  const iconItemClasses = [
+    'inline-flex flex-col justify-center items-center cursor-pointer', // 기본 설정
+    'gap-[0.5rem]', // mobile
+    'md:gap-[1rem] lg:gap-[1rem]', // tablet, desktop
+  ].join(' ');
 
   return (
     <div className={`${baseClasses} ${responsiveClasses} `} {...props}>
-      {/* Title */}
-      <div className={`${titleClasses}`}></div>
-      {/* Icon */}
-      <div className={`${iconContainerClasses}`}></div>
-      {/* Copyright */}
-      <div className={copyrightClasses}></div>
+      {/* Logo */}
+      <div className={`${logoClasses}`}>
+        <img src={menuMode === 'light' ? smwu_lion_logo_dark : smwu_lion_logo_light} alt="SMWU Like Lion " />
+      </div>
+      {/* MENU */}
+      {MENU_ORDER.map((item) => (
+        <div key={item.key} className="">
+          <Link to={item.path}>
+            {item.label}
+            {item.key === 'apply' && <BsArrowUpRightCircleFill className="ml-1 inline-block" />}
+          </Link>
+        </div>
+      ))}
+      {/* KAKAO, INSTAGRAM icon */}
+      <div className={`${iconContainerClasses}`}>
+        <a href={KAKAOTALK_URL} target="_blank" rel="noreferrer">
+          <RiKakaoTalkFill className={`${iconItemClasses}`} color={menuModeColor} />
+        </a>
+        <a href={INSTAGRAM_URL} target="_blank" rel="noreferrer">
+          <RiInstagramFill className={`${iconItemClasses}`} color={menuModeColor} />
+        </a>
+      </div>
     </div>
   );
 };
