3f547e1021f20f091015bfd0b20707380a47c122
ynzung <syj91105@sookmyung.ac.kr>
2025-11-29 00:23:41 +0900

    chore: OrbitSection의 orbit config 데이터 상수화(#22)

--- src/pages/about/constants/orbit.ts
+++ src/pages/about/constants/orbit.ts
@@ -2,3 +2,35 @@ export const ORBIT_DATA = {
   title: '함께여서 더 대담한 숙명',
   description: '개발이 낯선 비전공자도, 깊이를 찾는 전공자도\n함께라면 한계를 넘어설 수 있다고 믿습니다.',
 };
+
+export const ORBIT_CONFIG = {
+  desktop: [
+    { radius: 80, reverse: true, lineTransparency: '100' },
+    { radius: 145, reverse: true, lineTransparency: '90' },
+    { radius: 220, lineTransparency: '80', icons: ['blue', 'blue'] },
+    { radius: 285, reverse: true, lineTransparency: '60', icons: ['orange'] },
+    { radius: 350, lineTransparency: '40', icons: ['orange', 'orange', 'orange', 'blue'] },
+    { radius: 415, reverse: true, lineTransparency: '30', icons: ['orange', 'orange', 'blue'] },
+    { radius: 480 },
+  ],
+
+  tablet: [
+    { radius: 180, reverse: true, lineTransparency: '100' },
+    { radius: 270, reverse: true, lineTransparency: '90' },
+    { radius: 360, lineTransparency: '80', icons: ['blue', 'blue'] },
+    { radius: 450, reverse: true, lineTransparency: '60', icons: ['orange'] },
+    { radius: 540, lineTransparency: '40', icons: ['orange', 'orange', 'orange', 'blue'] },
+    { radius: 630, reverse: true, lineTransparency: '30', icons: ['orange', 'orange', 'blue'] },
+    { radius: 720 },
+  ],
+
+  mobile: [
+    { radius: 90, reverse: true, lineTransparency: '100' },
+    { radius: 153, reverse: true, lineTransparency: '90' },
+    { radius: 216, lineTransparency: '80', icons: ['blue', 'blue'] },
+    { radius: 279, reverse: true, lineTransparency: '60', icons: ['orange'] },
+    { radius: 342, lineTransparency: '40', icons: ['orange', 'orange', 'orange', 'blue'] },
+    { radius: 405, reverse: true, lineTransparency: '30', icons: ['orange', 'orange', 'blue'] },
+    { radius: 468 },
+  ],
+};
