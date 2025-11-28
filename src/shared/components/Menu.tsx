7397e2a49ac8ee449da2edc6634f8da0b93595f3
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-11-28 15:30:15 +0900

    chore: menu 컴포넌트 수정 중

---
+++ src/shared/components/Menu.tsx
@@ -0,0 +1,72 @@
+// import { useNavigate } from 'react-router-dom';
+
+export interface MenuProps {
+  /* 메뉴 모드 - light / dark */
+  mode?: 'light' | 'dark';
+}
+
+const Menu = ({ mode = 'light', ...props }: MenuProps) => {
+  // const navigate = useNavigate(); // TODO: 메뉴 네비게이션 구현 시 사용
+
+  /* menu 사용법
+  // 컴포넌트에서 사용
+{MENU_ORDER.map((item) => (
+  <Link key={item.key} to={item.path}>
+    {item.label}
+  </Link>
+))}
+
+// 특정 메뉴 접근
+const homeMenu = MENU_ITEMS.HOME;
+console.log(homeMenu.label); // '홈'
+
+  menu 사용법 끝 ~~~ 
+  ------- */
+
+  // 기본 스타일 + 색상 모드
+  const baseClasses = [
+    'w-screen inline-flex flex-col justify-center items-center',
+    `${mode === 'light' ? 'bg-white' : 'bg-navyblack  '}`,
+  ].join(' ');
+
+  // 반응형 padding, gap 스타일 (모바일 기본, md: 태블릿, lg: 데스크톱)
+  const responsiveClasses = [
+    'pt-[3.13rem] pb-[1.69rem] gap-[1.25rem]', // mobile
+    'md:pt-[5rem] md:pb-[3.19rem] md:gap-[2.5rem]', // tablet
+    'lg:pt-[8.13rem] lg:pb-[3.06rem] lg:gap-[3.06rem]', // desktop
+  ].join(' ');
+
+  // 반응형 타이틀 사이즈(모바일 기본, md: 태블릿, lg: 데스크톱)
+  const titleClasses = [
+    'font-semibold text-[1rem] md:text-[1.5rem] lg:text-[1.75rem] whitespace-pre-line text-center', //
+    `${mode === 'light' ? 'text-navyblack' : 'text-white  '}`, // 색상 모드
+  ].join(' ');
+
+  // 반응형 아이콘 컨테이너 사이즈
+  const iconContainerClasses = 'inline-flex flex-row justify-center items-center gap-[2rem]';
+  // TODO: 아이콘 메뉴 구현 시 사용할 스타일들
+  // const iconItemClasses = [
+  //   'inline-flex flex-col justify-center items-center cursor-pointer', // 기본 설정
+  //   'gap-[0.5rem]', // mobile
+  //   'md:gap-[1rem] lg:gap-[1rem]', // tablet, desktop
+  // ].join(' ');
+  // const iconTextClasses = 'font-semibold text-blue text-[0.875rem] md:text-[1rem] lg:text-[1rem]';
+  // const iconSizeClasses = 'text-blue w-[1.5rem] h-[1.5rem] md:w-[1.75rem] md:h-[1.75rem] lg:w-[2rem] lg:h-[2rem]';
+
+  // 반응형 저작권 표시 텍스트 사이즈
+  const copyrightClasses =
+    'text-center text-gray font-normal leading-[150%] text-[0.75rem] md:text-[0.875rem] lg:text-[1rem]';
+
+  return (
+    <div className={`${baseClasses} ${responsiveClasses} `} {...props}>
+      {/* Title */}
+      <div className={`${titleClasses}`}></div>
+      {/* Icon */}
+      <div className={`${iconContainerClasses}`}></div>
+      {/* Copyright */}
+      <div className={copyrightClasses}></div>
+    </div>
+  );
+};
+
+export default Menu;
