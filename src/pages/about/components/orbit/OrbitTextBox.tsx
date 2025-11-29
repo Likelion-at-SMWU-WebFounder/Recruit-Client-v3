b640a7129c7e960c026b7b22cbd2d45d9577547c
ynzung <syj91105@sookmyung.ac.kr>
2025-11-29 15:23:35 +0900

    chore: OribitTextBox 컴포넌트에 들어가는 데이터 about.ts에서 가져오기로 변경(#22)

--- src/pages/about/components/orbit/OrbitTextBox.tsx
+++ src/pages/about/components/orbit/OrbitTextBox.tsx
@@ -1,13 +1,13 @@
-import { ORBIT_DATA } from '@pages/about/constants/orbit';
+import { TITLE } from '@pages/about/constants/about';
 
 const OrbitTextBox = () => {
   return (
     <div className="absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2 space-y-[1rem]">
       <div className="text-navyblack text-center text-[1.75rem] leading-[150%] font-[700] tracking-[-0.088rem] md:text-[4rem]">
-        {ORBIT_DATA.title}
+        {TITLE.TITLE_2}
       </div>
       <p className="text-navyblack/70 text-center text-[1.125rem] leading-[170%] font-[500] whitespace-pre-line md:text-[2rem]">
-        {ORBIT_DATA.description}
+        {TITLE.DESCRIPTION_2}
       </p>
     </div>
   );
