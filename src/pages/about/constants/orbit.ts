98110d9df17f130c2e36edeb9bee8a45e5150856
ynzung <syj91105@sookmyung.ac.kr>
2025-11-29 15:22:43 +0900

    chore: orbit.ts에서 ORBIT_DATA 제거(#22)

--- src/pages/about/constants/orbit.ts
+++ src/pages/about/constants/orbit.ts
@@ -1,8 +1,3 @@
-export const ORBIT_DATA = {
-  title: '함께여서 더 대담한 숙명',
-  description: '개발이 낯선 비전공자도, 깊이를 찾는 전공자도\n함께라면 한계를 넘어설 수 있다고 믿습니다.',
-};
-
 export const ORBIT_CONFIG = {
   desktop: [
     { radius: 80, reverse: true, lineTransparency: '100' },
