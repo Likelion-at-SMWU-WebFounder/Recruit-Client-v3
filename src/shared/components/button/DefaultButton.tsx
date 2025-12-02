ed3acb28fc0e6dc80413fe44088322e35b81166e
ynzung <syj91105@sookmyung.ac.kr>
2025-12-02 14:35:06 +0900

    style: DefaultButton 컴포넌트 태블릿뷰 hover 제거(#24)

--- src/shared/components/button/DefaultButton.tsx
+++ src/shared/components/button/DefaultButton.tsx
@@ -14,7 +14,7 @@ export interface DefaultButtonProps {
 const DefaultButton = ({ border = 'solid', children, isIcon = true, onClick, ...props }: DefaultButtonProps) => {
   // 기본 스타일
   const baseClasses =
-    'bg-white text-blue hover:bg-blue hover:text-white inline-flex justify-center items-center gap-2.5 overflow-hidden cursor-pointer';
+    'bg-blue text-white lg:bg-white lg:text-blue lg:hover:bg-blue lg:hover:text-white inline-flex justify-center items-center gap-2.5 overflow-hidden cursor-pointer';
 
   // 반응형 버튼 사이즈(모바일 기본, md: 태블릿, lg: 데스크톱)
   const sizeClasses = [
