9ef5a5a42914f9c57280e0a676645a2012c7edeb
ynzung <syj91105@sookmyung.ac.kr>
2025-12-15 19:51:59 +0900

    chore: project 페이지 index 데이터 상수화(#32)

---
+++ src/pages/project/constants/index.ts
@@ -0,0 +1,31 @@
+// 프로젝트 타이틀, 서브타이틀
+export const PROJECT_TITLE = 'PROJECT';
+export const PROJECT_SUBTITLE = '아이디어를 현실로, 우리가 만든 변화';
+
+// 프로젝트 기수별 필터 옵션
+export const PROJECT_FILTER_OPTIONS = [
+  {
+    id: 1,
+    name: '전체',
+  },
+  {
+    id: 2,
+    name: '13기',
+  },
+  {
+    id: 3,
+    name: '12기',
+  },
+  {
+    id: 4,
+    name: '11기',
+  },
+  {
+    id: 5,
+    name: '10기',
+  },
+  {
+    id: 6,
+    name: '9기',
+  },
+];
