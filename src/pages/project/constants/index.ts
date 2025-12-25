26fcf46729113bd895f9c7afdd7df15249b223b5
ynzung <syj91105@sookmyung.ac.kr>
2025-12-25 18:09:30 +0900

    chore: 프로젝트 페이지 배경 이미지 경로 상수화(#36)

--- src/pages/project/constants/index.ts
+++ src/pages/project/constants/index.ts
@@ -1,6 +1,15 @@
+const PROJECT_IMAGES_PATH = import.meta.env.VITE_LOCAL_IMAGE_PATH + '/project';
+
 // 프로젝트 타이틀, 서브타이틀
 export const PROJECT_TITLE = 'PROJECT';
 export const PROJECT_SUBTITLE = '아이디어를 현실로, 우리가 만든 변화';
+
+// 프로젝트 페이지 배경 이미지 경로
+export const PROJECT_BACKGROUND_IMAGES_PATH = {
+  mobile: PROJECT_IMAGES_PATH + '/background-image/project-mobile.webp',
+  tablet: PROJECT_IMAGES_PATH + '/background-image/project-tablet.webp',
+  desktop: PROJECT_IMAGES_PATH + '/background-image/project-desktop.webp',
+};
 
 // 프로젝트 기수별 필터 옵션
 export const PROJECT_FILTER_OPTIONS = [
