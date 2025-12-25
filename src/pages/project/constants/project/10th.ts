d9e2d3f1833f4040e3498683e44444d02b355e3e
ynzung <syj91105@sookmyung.ac.kr>
2025-12-25 18:25:25 +0900

    chore: 프로젝트 데이터 content->description 필드명 수정(#36)

--- src/pages/project/constants/project/10th.ts
+++ src/pages/project/constants/project/10th.ts
@@ -7,7 +7,7 @@ export const tenthProjectsData = [
     title: '마이힐링 (MY Healing)',
     teamMember: '양지원(PM) 김수민(FE) 양은수(FE) 조은비(BE) 최예진(BE)',
     summary: '나만의 힐링 플레이스 공유 사이트',
-    content:
+    description:
       'MY_Healing은 힐링플레이스 검색 서비스로, 힐링 장소를 찾을 때 검색 시간을 단축하고 힐링 장소 선정에 편리함을 주는 서비스입니다. 여러 키워드를 제공하며, 가이드(ex. 비오는 날 가기 좋은 장소)를 제공하여 장소에 대한 구체적인 후기를 남길 수 있고, 추억도 남길 수 있습니다.',
     googleDriveUrl: null,
     gitOrgUrl: null,
@@ -24,7 +24,7 @@ export const tenthProjectsData = [
     title: 'rememB',
     teamMember: '고두영(FE) 나유진(FE) 박소윤(BE) 이수민(BE)',
     summary: '한 번 뿐인 생일을 더욱 특별하게, 마음을 전하는 rememB',
-    content: `내 생일파티에 초대할게! 내 파티룸을 꾸며줘💖
+    description: `내 생일파티에 초대할게! 내 파티룸을 꾸며줘💖
 
 1. 생일축하 편지 쓰고 파티룸 꾸미기 🎉
 2. 밸런스게임으로 내 선물취향 티내기 🎁
@@ -48,7 +48,7 @@ export const tenthProjectsData = [
     title: 'Otr.',
     teamMember: '김희원(PM) 배지윤(FE) 소희수(FE) 김지수(BE) 임아현(BE)',
     summary: '유서 작성 및 문답을 통한 디지털 임종 체험 서비스 Otr.',
-    content:
+    description:
       "죽음에 대한 생각을 정리할 수 있도록 도와줌으로써 삶을 다시 돌아볼 수 있게 해주는 디지털 임종 체험 서비스입니다. 서비스명인 'Otr.'은 'Over the rainbow'와 'Off the record'의 줄임말로, 죽음에 대해 생각해보며 나의 진솔한 생각을 돌아보자는 의미를 지닙니다. 7일간 죽음에 대해 생각해 볼 수 있는 문답을 작성하면, 이를 바탕으로 유서를 작성하여 스스로를 되돌아보고 궁극적으로 웰다잉을 추구하도록 돕습니다.",
     googleDriveUrl: null,
     gitOrgUrl: null,
