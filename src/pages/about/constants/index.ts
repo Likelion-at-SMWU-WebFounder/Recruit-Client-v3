a0debff82bccbef019dc0702d06f211fd388dc7a
ynzung <syj91105@sookmyung.ac.kr>
2025-12-30 00:08:39 +0900

    feat: about 페이지 HeroSection 배경 이미지 경로 상수화(#39)

--- src/pages/about/constants/index.ts
+++ src/pages/about/constants/index.ts
@@ -1,10 +1,19 @@
 // about 페이지에서 사용되는 title, subTitle 등의 상수 모음 파일
 
+const ABOUT_IMAGES_PATH = import.meta.env.VITE_IMAGE_PATH + '/about';
+
 export const TITLE = {
   TITLE_1: '아이디어를 서비스로, 캠퍼스에서 세상으로',
   DESCRIPTION_1: '숙명이 부르면, 멋대로 답한다',
   TITLE_2: '함께여서 더 대담한 숙명',
   DESCRIPTION_2: '개발이 낯선 비전공자도, 깊이를 찾는 전공자도\n함께라면 한계를 넘어설 수 있다고 믿습니다.',
+};
+
+// About 페이지 히어로 섹션 배경 이미지 경로
+export const HERO_BACKGROUND_IMAGES_PATH = {
+  mobile: ABOUT_IMAGES_PATH + '/background-image/hero-mobile.webp',
+  tablet: ABOUT_IMAGES_PATH + '/background-image/hero-tablet.webp',
+  desktop: ABOUT_IMAGES_PATH + '/background-image/hero-desktop.webp',
 };
 
 export const SUB_TITLE = {
