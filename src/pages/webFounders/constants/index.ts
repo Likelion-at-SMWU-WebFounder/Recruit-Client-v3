854392d9ceb035739d14bce4e2984ca5092614d6
ynzung <syj91105@sookmyung.ac.kr>
2025-12-29 23:07:46 +0900

    feat: Web Founders 페이지 상수 파일 추가(#39)

--- src/pages/webFounders/constants/index.ts
+++ src/pages/webFounders/constants/index.ts
@@ -1,137 +1,21 @@
+import type { WebFoundersFilterOptionsType } from '@/pages/webFounders/types/founder';
+
 // 웹파운더즈 타이틀, 서브타이틀
 export const WEBFOUNDERS_TITLE = 'Web Founders';
 export const WEBFOUNDERS_SUBTITLE = '숙명여자대학교 멋사 홈페이지 제작자를 소개합니다';
 
-// 웹파운더즈 이미지 경로
-const WEBFOUNDERS_IMAGE_PATH = import.meta.env.VITE_IMAGE_PATH + '/webfounders/members';
-
-// 웹파운더즈: 기획디자인 멤버
-export const WEBFOUNDERS_PLAN_DESIGN = [
+// 웹파운더즈 기수별 필터 옵션
+export const WEBFOUNDERS_FILTER_OPTIONS = [
   {
     id: 1,
-    name: '강주은',
-    no: '12기',
-    part: '프론트엔드',
-    image: WEBFOUNDERS_IMAGE_PATH + '/KangJuEun.webp',
-    responsibilities: '기획디자인 파트 담당',
-    instagram: '',
-    email: '',
-    github: '',
+    name: '3기',
   },
   {
     id: 2,
-    name: '김보미',
-    no: '12기',
-    part: '기획디자인',
-    image: WEBFOUNDERS_IMAGE_PATH + '/KimBoMi.webp',
-    responsibilities: '기획디자인 파트 담당',
-    instagram: '',
-    email: '',
-    github: '',
-  },
-
-  {
-    id: 3,
-    name: '박우현',
-    no: '12기',
-    part: '기획디자인',
-    image: WEBFOUNDERS_IMAGE_PATH + '/ParkWooHyun.webp',
-    responsibilities: '기획디자인 파트 담당',
-    instagram: '',
-    email: '',
-    github: '',
-  },
-];
-
-// 웹파운더즈: 프론트엔드 멤버
-export const WEBFOUNDERS_FRONTEND = [
-  {
-    id: 1,
-    name: '성윤정',
-    no: '12기',
-    part: '프론트엔드',
-    image: WEBFOUNDERS_IMAGE_PATH + '/SeongYunJung.webp',
-    responsibilities: '프론트엔드 파트 담당',
-    instagram: '',
-    email: '',
-    github: '',
-  },
-  {
-    id: 2,
-    name: '유동은',
-    no: '12기',
-    part: '프론트엔드',
-    image: WEBFOUNDERS_IMAGE_PATH + '/YouDongEun.webp',
-    responsibilities: '프론트엔드 파트 담당',
-    instagram: '',
-    email: '',
-    github: '',
+    name: '2기',
   },
   {
     id: 3,
-    name: '이현정',
-    no: '12기',
-    part: '프론트엔드',
-    image: WEBFOUNDERS_IMAGE_PATH + '/LeeHyeonJeong.webp',
-    responsibilities: '프론트엔드 파트 담당',
-    instagram: '',
-    email: '',
-    github: '',
+    name: '1기',
   },
-];
-
-// 웹파운더즈: 백엔드 멤버
-export const WEBFOUNDERS_BACKEND = [
-  {
-    id: 1,
-    name: '경민서',
-    no: '12기',
-    part: '백엔드',
-    image: WEBFOUNDERS_IMAGE_PATH + '/KyungMinSeo.webp',
-    responsibilities: '백엔드 파트 담당',
-  },
-  {
-    id: 2,
-    name: '서문지',
-    no: '12기',
-    part: '백엔드',
-    image: WEBFOUNDERS_IMAGE_PATH + '/SeoMunJi.webp',
-    responsibilities: '백엔드 파트 담당',
-    instagram: '',
-    email: '',
-    github: '',
-  },
-  {
-    id: 3,
-    name: '유채민',
-    no: '12기',
-    part: '백엔드',
-    image: WEBFOUNDERS_IMAGE_PATH + '/YuChaeMin.webp',
-    responsibilities: '백엔드 파트 담당',
-    instagram: '',
-    email: '',
-    github: '',
-  },
-  {
-    id: 4,
-    name: '정지윤',
-    no: '12기',
-    part: '백엔드',
-    image: WEBFOUNDERS_IMAGE_PATH + '/JungJiYoon.webp',
-    responsibilities: '백엔드 파트 담당',
-    instagram: '',
-    email: '',
-    github: '',
-  },
-  {
-    id: 5,
-    name: '홍상희',
-    no: '12기',
-    part: '백엔드',
-    image: WEBFOUNDERS_IMAGE_PATH + '/HongSangHee.webp',
-    responsibilities: '백엔드 파트 담당',
-    instagram: '',
-    email: '',
-    github: '',
-  },
-];
+] as WebFoundersFilterOptionsType[];
