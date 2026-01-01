58c212db3b6d5aa59dcb62a1dad58a1848afb14b
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-01 11:23:03 +0900

    add: 메뉴 제거 후 Layout 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,12 @@
-import Menu from '@components/Menu';
+import Layout from '@shared/components/Layout';
 
 const Home = () => {
   return (
-    <>
-      <Menu />
-      숙멋 홈페이지
-    </>
+    <Layout menuMode="dark" footerMode="dark">
+      <div className="bg-navyblack color-white flex h-[200vh] items-center justify-center bg-black text-white">
+        숙멋 홈페이지
+      </div>
+    </Layout>
   );
 };
 
