d60eebee9bf366c5100bfe8939325030b378f923
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-01 11:22:43 +0900

    feat: 메뉴, 푸터가 들어간 layout 구현 작업 중

---
+++ src/shared/components/Layout.tsx
@@ -0,0 +1,21 @@
+import { type ReactNode } from 'react';
+import Menu from '@components/Menu';
+import Footer from '@components/Footer';
+
+export interface LayoutProps {
+  menuMode?: 'light' | 'dark';
+  footerMode?: 'light' | 'dark';
+  children: ReactNode;
+}
+
+const Layout = ({ menuMode = 'light', footerMode = 'light', children }: LayoutProps) => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Menu mode={menuMode} />
+      <main className="flex-1">{children}</main>
+      <Footer mode={footerMode} />
+    </div>
+  );
+};
+
+export default Layout;
