d23dad4ecbd5ab5f22323aec80ef515f175b9914
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-01 13:30:40 +0900

    design: 배경 블러, 색상 모드에 따른 그림자 추가 (#17)

--- src/shared/components/Menu.tsx
+++ src/shared/components/Menu.tsx
@@ -12,55 +12,68 @@ export interface MenuProps {
 
 const Menu = ({ mode = 'light', ...props }: MenuProps) => {
   // 메뉴 모드에 따른 글, 아이콘 색상 설정
-  const menuModeColor = mode === 'light' ? 'navyblack' : 'white';
+  const isLightMode = mode === 'light';
+  const menuItemColor = isLightMode ? 'navyblack' : 'white';
 
   // 기본 스타일 + 색상 모드
   const baseClasses = [
-    'fixed top-0 left-0 w-screen flex flex-row justify-between items-center z-50',
-    'backdrop-blur-md bg-white/10 dark:bg-black/80',
-    `${mode === 'light' ? 'text-navyblack' : 'text-white'}`,
+    'fixed top-0 left-0 w-screen inline-flex flex-row justify-between items-center z-50',
+    'backdrop-blur shadow-xl ',
+    `text-${menuItemColor}`,
+    `${isLightMode ? 'shadow-gray/5' : 'shadow-black/10'}`,
   ].join(' ');
 
   // 반응형 padding, gap 스타일 (모바일 기본, md: 태블릿, lg: 데스크톱)
   const responsiveClasses = [
-    'pt-[1rem] pb-[1.69rem] gap-[1.25rem]', // mobile
-    'md:pt-[5rem] md:pb-[3.19rem] md:gap-[2.5rem]', // tablet
-    'lg:pt-[8.13rem] lg:pb-[3.06rem] lg:gap-[3.06rem]', // desktop
+    'p-[1rem] gap-[1.25rem]', // mobile
+    'md:gap-[2.5rem]', // tablet
+    'lg:gap-[3.06rem]', // desktop
   ].join(' ');
 
-  // 반응형 타이틀 사이즈(모바일 기본, md: 태블릿, lg: 데스크톱)
-  const logoClasses = 'h-[34px], md:h-[42px], lg:h-[60px]';
+  // 반응형 로고 사이즈
+  const logoClasses = 'h-[5rem], md:h-[5rem], lg:h-[5rem]';
+
+  // 반응형 메뉴 사이즈
+  const menuContainerClasses = 'w-[50%] inline-flex flex-row justify-between items-center';
+  const menuItemClasses = [
+    'font-semibold text-[1rem] md:text-[1.25rem] lg:text-[1.5rem]', // mobile, tablet, desktop
+    'hover:underline underline-offset-8', // hover 효과
+  ].join(' ');
 
   // 반응형 아이콘 컨테이너 사이즈
-  const iconContainerClasses = 'inline-flex flex-row justify-center items-center gap-[2rem]';
+  const iconContainerClasses = 'inline-flex flex-row justify-center items-right gap-[0.5rem] md:gap-[1rem]';
   const iconItemClasses = [
     'inline-flex flex-col justify-center items-center cursor-pointer', // 기본 설정
-    'gap-[0.5rem]', // mobile
-    'md:gap-[1rem] lg:gap-[1rem]', // tablet, desktop
+    'h-[2rem] gap-[0.5rem]', // mobile
+    'md:h-[4rem] md:gap-[1rem] lg:gap-[1rem]', // tablet, desktop
   ].join(' ');
 
   return (
-    <div className={`${baseClasses} ${responsiveClasses} `} {...props}>
+    <div className={`${baseClasses} ${responsiveClasses}`} {...props}>
       {/* Logo */}
-      <div className={`${logoClasses}`}>
-        <img src={mode === 'light' ? smwu_lion_logo_dark : smwu_lion_logo_light} alt="SMWU Like Lion " />
-      </div>
+      <img
+        className={logoClasses}
+        src={mode === 'light' ? smwu_lion_logo_dark : smwu_lion_logo_light}
+        alt="SMWU Like Lion"
+      />
       {/* MENU */}
-      {MENU_ORDER.map((item) => (
-        <div key={item.key} className="">
-          <Link to={item.path}>
-            {item.label}
-            {item.key === 'apply' && <BsArrowUpRightCircleFill className="ml-1 inline-block" />}
-          </Link>
-        </div>
-      ))}
+      <div className={menuContainerClasses}>
+        {MENU_ORDER.map((item) => (
+          <span key={item.key} className={menuItemClasses}>
+            <Link to={item.path}>
+              {item.label}
+              {item.key === 'apply' && <BsArrowUpRightCircleFill className="ml-1 inline-block" />}
+            </Link>
+          </span>
+        ))}
+      </div>
       {/* KAKAO, INSTAGRAM icon */}
-      <div className={`${iconContainerClasses}`}>
+      <div className={iconContainerClasses}>
         <a href={KAKAOTALK_URL} target="_blank" rel="noreferrer">
-          <RiKakaoTalkFill className={`${iconItemClasses}`} color={menuModeColor} />
+          <RiKakaoTalkFill className={`${iconItemClasses}`} color={menuItemColor} />
         </a>
         <a href={INSTAGRAM_URL} target="_blank" rel="noreferrer">
-          <RiInstagramFill className={`${iconItemClasses}`} color={menuModeColor} />
+          <RiInstagramFill className={`${iconItemClasses}`} color={menuItemColor} />
         </a>
       </div>
     </div>
