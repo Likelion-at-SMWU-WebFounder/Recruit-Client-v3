c1bb493d86ac0c01873f1a3612b1e885eec71b93
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-04 02:03:41 +0900

    add: home 페이지에 layout 추가 (#17)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,11 @@
 import Layout from '@shared/components/Layout';
+import HeroSection from '@pages/home/components/hero/HeroSection';
 
 const Home = () => {
   return (
-    <Layout menuMode="dark" footerMode="dark">
-      <div className="bg-navyblack color-white flex h-[200vh] items-center justify-center bg-black text-white">
-        숙멋 홈페이지
-      </div>
+    <Layout menuMode="light" footerMode="light">
+      <HeroSection />
+      <div className="flex h-[200vh] items-center justify-center">숙멋 홈페이지</div>
     </Layout>
   );
 };
