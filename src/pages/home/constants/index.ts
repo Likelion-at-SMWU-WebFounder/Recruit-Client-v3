efebd0b780f8271ffbdae93031af429420ca8422
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-04 02:02:31 +0900

    chore: home 페이지 hero 섹션 배경 이미지 경로 상수 추가

---
+++ src/pages/home/constants/index.ts
@@ -0,0 +1,8 @@
+const HOME_IMAGES_PATH = import.meta.env.VITE_IMAGE_PATH + '/home';
+
+// home 페이지 hero 섹션 배경 이미지 경로
+export const HERO_BACKGROUND_IMAGES_PATH = {
+  mobile: HOME_IMAGES_PATH + '/background-image/hero-mobile.webp',
+  tablet: HOME_IMAGES_PATH + '/background-image/hero-tablet.webp',
+  desktop: HOME_IMAGES_PATH + '/background-image/hero-desktop.webp',
+};
