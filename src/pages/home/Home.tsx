92402a5c0e8ec9bb1d20628794cdce66d7135d8d
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-04 18:40:07 +0900

    chore: home 페이지 파일에 aboutsection 추가 (#67)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,12 @@
 import Layout from '@shared/components/Layout';
 import HeroSection from '@pages/home/components/hero/HeroSection';
+import AboutSection from '@pages/home/components/about/AboutSection';
 
 const Home = () => {
   return (
     <Layout menuMode="light" footerMode="light">
       <HeroSection />
-      <div className="flex h-[200vh] items-center justify-center">숙멋 홈페이지</div>
+      <AboutSection />
     </Layout>
   );
 };
