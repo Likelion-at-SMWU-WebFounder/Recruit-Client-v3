6b957e7cafd429c1225794ae34925e45f20cabae
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-04 23:49:41 +0900

    refactor, design: 모바일 메뉴 서랍 컴포넌트 분리 및 디자인 오류 수정 (#17)

--- src/shared/components/Menu.tsx
+++ src/shared/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { MENU_ORDER } from '@shared/constants/menu';
 import { KAKAOTALK_URL, INSTAGRAM_URL } from '@shared/constants/url';
@@ -9,15 +9,77 @@ export interface MenuProps {
   mode?: 'light' | 'dark';
 }
 
+const mobileMenuColor = 'text-navyblack';
+const hoverColor = 'hover:text-blue';
+
+// 모바일 메뉴 서랍 컴포넌트
+export interface MobileDrawerProps {
+  setDrawerOpen: (open: boolean) => void;
+  mobileMenuColor: string;
+}
+
+const MobileDrawer = ({ setDrawerOpen, mobileMenuColor }: MobileDrawerProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
+  const handleClose = () => {
+    setIsVisible(false);
+    setTimeout(() => setDrawerOpen(false), 300);
+  };
+
+  const mobileMenuContainerClasses = `
+    fixed top-0 right-0 h-screen w-full inline-flex flex-col justify-start items-start z-[200] bg-white md:hidden
+    transform transition-transform duration-300 ease-in-out
+    ${isVisible ? 'translate-x-0' : 'translate-x-full'}
+  `;
+  const mobileMenuListClasses = 'm-[2.5rem_1rem] flex flex-col gap-[2rem] text-[1.5rem] font-medium';
+  const mobileMenuItemClasses = `${mobileMenuColor} ${hoverColor} w-[100dvw] cursor-pointer text-[1.5rem] font-medium`;
+  const mobileIconContainerClasses = 'inline-flex gap-[1rem]';
+  const mobileIconItemClasses = `${mobileMenuColor} ${hoverColor} cursor-pointer h-[2rem] w-[2rem]`;
+
+  return (
+    <>
+      <div className={mobileMenuContainerClasses}>
+        <span className={`inline-flex h-[2.5rem] w-full justify-end p-[0.625rem_1rem]`}>
+          <RiCloseLine
+            className={`${mobileMenuColor} ${hoverColor} cursor-pointer md:h-[2rem] md:w-[2rem] lg:h-[2rem] lg:w-[2rem]`}
+            onClick={handleClose}
+          />
+        </span>
+        <div className={mobileMenuListClasses}>
+          {MENU_ORDER.map((item) => (
+            <span key={item.key} className={mobileMenuItemClasses}>
+              <Link to={item.path} onClick={handleClose}>
+                {item.label}
+              </Link>
+            </span>
+          ))}
+          <div className={mobileIconContainerClasses}>
+            <a href={KAKAOTALK_URL} target="_blank" rel="noreferrer">
+              <RiKakaoTalkFill className={mobileIconItemClasses} />
+            </a>
+            <a href={INSTAGRAM_URL} target="_blank" rel="noreferrer">
+              <RiInstagramFill className={mobileIconItemClasses} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
 const Menu = ({ mode = 'light', ...props }: MenuProps) => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false);
   const isLightMode = mode === 'light';
-  const menuItemColor = isLightMode ? 'navyblack' : 'white';
+  const menuModeColor = isLightMode ? 'text-navyblack' : 'text-white';
 
   const baseClasses = [
     'fixed top-0 left-0 w-full inline-flex justify-between items-center z-[100]',
     'backdrop-blur-md shadow-xl shadow-black/10',
-    `${isLightMode ? 'text-navyblack' : 'text-white'}`,
+    `${menuModeColor}`,
   ].join(' ');
 
   const responsiveClasses = [
@@ -30,82 +92,53 @@ const Menu = ({ mode = 'light', ...props }: MenuProps) => {
 
   const menuListClasses =
     'hidden md:p-[0.625rem] md:inline-flex md:justify-center md:items-center md:gap-[2.5rem] lg:gap-[4rem]';
-  const menuItemClasses = 'md:text-[1rem] lg:text-[1.25rem] font-medium hover:underline underline-offset-8';
-
+  const menuItemClasses = `${hoverColor} md:text-[1rem] lg:text-[1.25rem] font-medium`;
   const iconContainerClasses = 'hidden md:inline-flex md:justify-center md:items-right md:gap-[1rem]';
-  const iconItemClasses = 'cursor-pointer md:h-[1.42rem] md:w-[1.42rem] lg:h-[2rem] lg:w-[2rem]';
-
-  const mobileMenuContainerClasses =
-    'fixed top-0 left-0 h-screen w-full inline-flex flex-col justify-left items-start z-[200] bg-white md:hidden';
-  const mobileMenuListClasses = 'm-[2.5rem_1rem] inline-flex flex-col gap-[2rem] text-[1.5rem] font-medium';
-  const mobileMenuItemClasses = 'text-[1.5rem] font-medium hover:underline underline-offset-8';
-  const mobileIconContainerClasses = 'inline-flex gap-[1rem]';
-  const mobileIconItemClasses = 'cursor-pointer h-[2rem] w-[2rem]';
-  const mobileIconColor = 'navyblack';
+  const iconItemClasses = `${menuModeColor} cursor-pointer md:h-[1.42rem] md:w-[1.42rem] lg:h-[2rem] lg:w-[2rem]`;
 
   return (
-    <div className={`${baseClasses} ${responsiveClasses}`} {...props}>
-      {/* Logo */}
-      <Link to="/">
-        <img className={logoClasses} src={isLightMode ? logo.dark : logo.light} alt="SMWU Like Lion" />
-      </Link>
-      {/* LIST */}
-      <div className={menuListClasses}>
-        {MENU_ORDER.map((item) => (
-          <span key={item.key} className={menuItemClasses}>
-            <Link to={item.path}>
-              {item.label}
-              {item.key === 'apply' && (
-                <img
-                  className={`${iconItemClasses} inline-block pl-[0.3rem]`}
-                  src={isLightMode ? apply_arrow.dark : apply_arrow.light}
-                  alt=" "
-                />
-              )}
-            </Link>
-          </span>
-        ))}
-      </div>
-      {/* LINK */}
-      <div className={iconContainerClasses}>
-        <a href={KAKAOTALK_URL} target="_blank" rel="noreferrer">
-          <RiKakaoTalkFill className={iconItemClasses} color={menuItemColor} />
-        </a>
-        <a href={INSTAGRAM_URL} target="_blank" rel="noreferrer">
-          <RiInstagramFill className={iconItemClasses} color={menuItemColor} />
-        </a>
-      </div>
-      {/* MOBILE MENU */}
-      <button className="cursor-pointer md:hidden" onClick={() => setMenuOpen(true)}>
-        <RiMenuLine className={iconItemClasses} color={menuItemColor} />
-      </button>
-      {menuOpen && (
-        <div className={mobileMenuContainerClasses}>
-          <button
-            className={`inline-flex ${responsiveClasses} w-full cursor-pointer justify-end`}
-            onClick={() => setMenuOpen(false)}>
-            <RiCloseLine className={iconItemClasses} color={menuItemColor} />
-          </button>
-          <div className={mobileMenuListClasses}>
-            {MENU_ORDER.map((item) => (
-              <span key={item.key} className={mobileMenuItemClasses}>
-                <Link to={item.path} onClick={() => setMenuOpen(false)}>
-                  {item.label}
-                </Link>
-              </span>
-            ))}
-            <div className={mobileIconContainerClasses}>
-              <a href={KAKAOTALK_URL} target="_blank" rel="noreferrer">
-                <RiKakaoTalkFill className={mobileIconItemClasses} color={mobileIconColor} />
-              </a>
-              <a href={INSTAGRAM_URL} target="_blank" rel="noreferrer">
-                <RiInstagramFill className={mobileIconItemClasses} color={mobileIconColor} />
-              </a>
-            </div>
-          </div>
+    <>
+      <div className={`${baseClasses} ${responsiveClasses}`} {...props}>
+        {/* 로고 : desktop, tablet, mobile */}
+        <Link to="/">
+          <img className={logoClasses} src={isLightMode ? logo.dark : logo.light} alt="SMWU Like Lion" />
+        </Link>
+
+        {/* 메뉴 리스트 : desktop, tablet */}
+        <div className={menuListClasses}>
+          {MENU_ORDER.map((item) => (
+            <span key={item.key} className={menuItemClasses}>
+              <Link to={item.path}>
+                {item.label}
+                {item.key === 'apply' && (
+                  <img
+                    className={`${iconItemClasses} inline-block pl-[0.3rem]`}
+                    src={isLightMode ? apply_arrow.dark : apply_arrow.light}
+                    alt=" "
+                  />
+                )}
+              </Link>
+            </span>
+          ))}
         </div>
-      )}
-    </div>
+
+        {/* 카카오톡, 인스타그램 아이콘 : desktop, tablet */}
+        <div className={iconContainerClasses}>
+          <a href={KAKAOTALK_URL} target="_blank" rel="noreferrer">
+            <RiKakaoTalkFill className={iconItemClasses} />
+          </a>
+          <a href={INSTAGRAM_URL} target="_blank" rel="noreferrer">
+            <RiInstagramFill className={iconItemClasses} />
+          </a>
+        </div>
+
+        {/* 햄버거 메뉴 아이콘 : mobile */}
+        <button className="cursor-pointer md:hidden" onClick={() => setDrawerOpen(true)}>
+          <RiMenuLine className={iconItemClasses} />
+        </button>
+        {drawerOpen && <MobileDrawer setDrawerOpen={setDrawerOpen} mobileMenuColor={mobileMenuColor} />}
+      </div>
+    </>
   );
 };
 
