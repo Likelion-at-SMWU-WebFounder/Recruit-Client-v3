d972d9da89b6a35e66b3488571eecd24ab317bea
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-06 00:07:28 +0900

    refactor: 객체 형태를 배열 형태로 수정 (#67)

--- src/pages/home/constants/about.ts
+++ src/pages/home/constants/about.ts
@@ -1,12 +1,12 @@
 // home 페이지 about 섹션 이미지 경로
 export const ABOUT_IMAGE_PATH = import.meta.env.VITE_IMAGE_PATH + '/home/about';
 
-export const ABOUT_US_IMAGES_PATH = {
-  1: ABOUT_IMAGE_PATH + '/about1.webp',
-  2: ABOUT_IMAGE_PATH + '/about2.webp',
-  3: ABOUT_IMAGE_PATH + '/about3.webp',
-  4: ABOUT_IMAGE_PATH + '/about4.webp',
-  5: ABOUT_IMAGE_PATH + '/about5.webp',
-  6: ABOUT_IMAGE_PATH + '/about6.webp',
-  7: ABOUT_IMAGE_PATH + '/about7.webp',
-};
+export const ABOUT_US_IMAGES_PATH = [
+  ABOUT_IMAGE_PATH + '/about1.webp',
+  ABOUT_IMAGE_PATH + '/about2.webp',
+  ABOUT_IMAGE_PATH + '/about3.webp',
+  ABOUT_IMAGE_PATH + '/about4.webp',
+  ABOUT_IMAGE_PATH + '/about5.webp',
+  ABOUT_IMAGE_PATH + '/about6.webp',
+  ABOUT_IMAGE_PATH + '/about7.webp',
+];
