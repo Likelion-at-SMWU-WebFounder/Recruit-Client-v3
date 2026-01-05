236192d8df1185df7c0f89baa5b8aca8a5ca5917
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-06 00:51:13 +0900

    chore: AboutCard 추가 및 반응형에 따라 section 분리 (#67)

--- src/pages/home/components/about/AboutSection.tsx
+++ src/pages/home/components/about/AboutSection.tsx
@@ -1,26 +1,32 @@
 import SubTitle from '@shared/components/SubTitle';
 import { SUB_TITLE, SUB_CONTENT_CLASSES } from '@pages/home/constants/index';
-// import { ABOUT_IMAGE_PATH } from '@pages/home/constants/about';
+import AboutCard from '@pages/home/components/about/AboutCard';
 
 const AboutSection = () => {
   const DESKTOP_TABLET_SECTION_CLASS =
-    'hidden h-[100dvh] w-full max-w-[100vw] md:flex md:flex-col md:items-center md:p-[10.5rem_2rem]';
+    'hidden w-full max-w-[100vw] md:flex md:flex-col md:items-center md:p-[10.5rem_2rem]';
   const MOBILE_SECTION_CLASS =
     'relative flex w-full max-w-[100vw] flex-col items-center justify-center gap-[3.38rem] py-[3.12rem] md:hidden';
 
   return (
     <>
-      {/* 태블릿/데스크톱: 카드 */}
+      {/* 데스크톱, 태블릿 */}
       <section className={DESKTOP_TABLET_SECTION_CLASS}>
         <div className="inline-flex flex-col items-center justify-center gap-[3.125rem]">
           <SubTitle subTitle={SUB_TITLE.SUB_TITLE_1} subDescription={SUB_TITLE.SUB_DESCRIPTION_1} />
           <div className={`${SUB_CONTENT_CLASSES}`}>{SUB_TITLE.SUB_CONTENT_1}</div>
         </div>
-        <div className=""></div>
+        <AboutCard />
       </section>
 
-      {/* 모바일: 카드 */}
-      <section className={MOBILE_SECTION_CLASS}></section>
+      {/* 모바일 */}
+      <section className={MOBILE_SECTION_CLASS}>
+        <div className="inline-flex flex-col items-center justify-center gap-[3.125rem]">
+          <SubTitle subTitle={SUB_TITLE.SUB_TITLE_1} subDescription={SUB_TITLE.SUB_DESCRIPTION_1} />
+          <AboutCard />
+          <div className={`${SUB_CONTENT_CLASSES}`}>{SUB_TITLE.SUB_CONTENT_1}</div>
+        </div>
+      </section>
     </>
   );
 };
