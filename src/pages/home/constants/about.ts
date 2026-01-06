ed36295ccf35b40df44b79d9bfba8e4b42b4e560
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-07 00:50:40 +0900

    add: ABOUT_BUTTON_TEXT에 데스크탑,테블릿 / 모바일 객체 추가 (#67)

--- src/pages/home/constants/about.ts
+++ src/pages/home/constants/about.ts
@@ -11,7 +11,7 @@ export const ABOUT_US_IMAGES_PATH = [
   ABOUT_IMAGE_PATH + '/about7.webp',
 ];
 
-export const ABOUT_BUTTON_TEXT = '더 알아보기';
+export const ABOUT_BUTTON_TEXT = { desktop_tablet: '더 알아보기', mobile: '동아리 더 알아보기' };
 
 export const ABOUT_CONTENT_TEXT = {
   desktop:
@@ -19,5 +19,5 @@ export const ABOUT_CONTENT_TEXT = {
   tablet:
     '숙명여자대학교 멋쟁이사자처럼은 전국 대학 연합 개발·창업 동아리인\n‘멋쟁이사자처럼’의 숙명여대 지부로, 올해 14기를 맞이했습니다.\n웹 서비스 기획부터 개발까지 실무 경험을 쌓을 수 있는 활동 중심의 커뮤니티입니다.',
   mobile:
-    '숙명여자대학교 멋쟁이사자처럼은 전국 대학 연합\n개발·창업 동아리인 ‘멋쟁이사자처럼’의\n숙명여대 지부로,올해 14기를 맞이했습니다.\n\n웹 서비스 기획부터 개발까지 실무 경험을 쌓을 수\n있는 활동 중심의 커뮤니티입니다.',
+    '숙명여자대학교 멋쟁이사자처럼은 전국 대학 연합\n개발·창업 동아리인 ‘멋쟁이사자처럼’의\n숙명여대 지부로, 올해 14기를 맞이했습니다.\n\n웹 서비스 기획부터 개발까지 실무 경험을 쌓을 수\n있는 활동 중심의 커뮤니티입니다.',
 };
