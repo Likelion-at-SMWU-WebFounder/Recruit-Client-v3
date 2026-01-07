c5b4e875df2aa61ccda3d10b92945d5233ce8f8f
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-08 00:35:58 +0900

    chore: activity 카드 관련 상수 추가 (#75)

--- src/pages/home/constants/activity.ts
+++ src/pages/home/constants/activity.ts
@@ -6,3 +6,16 @@ export const ACTIVITY_CONTENT_TEXT = {
   mobile:
     '정기 세미나부터 해커톤,\n파이널 프로젝트까지\n숙멋사의 활동은 도전으로 가득합니다.\n실무 중심의 커리큘럼과 협업 환경 속에서\n직접 아이디어를 실현하고 부딪치며 성장합니다.\n혼자가 아닌, 함께라서 더 큰 변화를 만들어냅니다.',
 };
+
+const ACTIVITY_IMAGES_PATH = import.meta.env.VITE_IMAGE_PATH + '/activity';
+
+export const ACTIVITY_CARD = {
+  hackathon: '해커톤',
+  hackathon_description: '해커톤 참여를 통해 아이디어를 기획부터 구현까지 빠르게 실현합니다.',
+  hackathon_icon: ACTIVITY_IMAGES_PATH + '/hackathon_icon.svg',
+  hackathon_image: ACTIVITY_IMAGES_PATH + '/hackathon.webp',
+  seminar: '세미나',
+  seminar_description: '매주 화요일 7시부터 9시까지 각 파트별로 정기 세미나를 진행합니다.',
+  seminar_icon: ACTIVITY_IMAGES_PATH + '/seminar_icon.svg',
+  seminar_image: ACTIVITY_IMAGES_PATH + '/seminar.webp',
+};
