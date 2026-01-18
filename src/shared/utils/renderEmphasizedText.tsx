5aba6fa1f1d3a26c6f272836cfd6b004d244bba0
ynzung <syj91105@sookmyung.ac.kr>
2026-01-18 23:10:46 +0900

    refactor: About 페이지 디자인 토큰 적용(#86)

--- src/shared/utils/renderEmphasizedText.tsx
+++ src/shared/utils/renderEmphasizedText.tsx
@@ -6,7 +6,7 @@ type RenderEmphasizedTextOptions = {
 };
 
 export const renderEmphasizedText = (text: string, options: RenderEmphasizedTextOptions = {}): ReactNode[] => {
-  const { emphasisClassName = 'font-[700] md:font-[800]', partClassName = 'text-gray' } = options;
+  const { emphasisClassName = 'font-[600] md:font-[800]', partClassName = 'text-gray' } = options;
 
   // 별(*) 강조 텍스트와 괄호 형식 텍스트를 모두 자동으로 처리
   return text.split(/(\*[^*]+\*|\([A-Z]+\))/g).map((segment, index) => {
