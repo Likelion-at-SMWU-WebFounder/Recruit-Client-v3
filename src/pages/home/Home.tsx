6f885216eac6608f4e652152efc2b31dbbca193b
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-25 08:16:17 +0900

    fix: Home 페이지 내 AboutSection에서만 menuMode가 light가 되도록 수정 (#75)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,42 +7,27 @@ import ActivitySection from '@pages/home/components/activity/ActivitySection';
 const Home = () => {
   const sectionClasses = 'relative h-[100dvh] snap-start overflow-hidden';
 
-  // 페이지 이동 시 메뉴 모드 변경을 위한 상태 (기본값 light)
   const [menuMode, setMenuMode] = useState<'light' | 'dark'>('dark');
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  // AboutSection이 뷰포트에 있는지 감지 -> menuMode 변경
   useEffect(() => {
-    const root = document.documentElement;
-    root.classList.add('scrollbar-hidden');
-    return () => {
-      root.classList.remove('scrollbar-hidden');
-    };
-  }, []);
-
-  useEffect(() => {
-    // 스크롤 위치에 따라 메뉴 모드 변경
     const aboutSection = document.getElementById('about-section');
-
     if (!aboutSection) return;
-    // 메뉴 모드 변경을 위한 IntersectionObserver 생성
+
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          const targetId = entry.target.id;
-
-          if (entry.isIntersecting) {
-            if (targetId === 'about-section') {
-              setMenuMode(entry.isIntersecting ? 'light' : 'dark'); // about 섹션에서는 라이트 모드
-            } else {
-              setMenuMode('dark');
-            }
-          }
+          // AboutSection 뷰포트에 있으면 light, 없으면 dark
+          setMenuMode(entry.isIntersecting ? 'light' : 'dark');
         });
       },
-      { threshold: 0.9 }
+      {
+        threshold: 0.9, // 90% 이상 보이면 감지
+      }
     );
 
     observer.observe(aboutSection);
