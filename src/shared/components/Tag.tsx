76fddb4e273af1b7685ab6360581dbc30017aea5
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-27 05:05:00 +0900

    design: Tag의 배경색 변경 (#138)

--- src/shared/components/Tag.tsx
+++ src/shared/components/Tag.tsx
@@ -9,14 +9,14 @@ const Tag = ({ children, mode = 'blue' }: TagProps) => {
   const baseClasses = 'inline-flex justify-center items-center rounded-[1.875rem] leading-[140%] font-semibold border';
 
   const blueClasses = [
-    'border-blue text-blue bg-white',
+    'border-blue text-blue bg-white-opacity12',
     'lg:text-[1rem] lg:py-[0.3125rem] lg:px-[0.9375rem]',
     'md:text-[0.625rem] md:py-[0.16669rem] md:px-[0.5rem]',
     'text-[0.4375rem] py-[0.11856rem] px-[0.35563rem]',
   ].join(' ');
 
   const lightDarkModeClasses = [
-    `${mode === 'light' && 'border-navyblack text-navyblack bg-white'}`,
+    `${mode === 'light' && 'border-navyblack text-navyblack bg-white-opacity12'}`,
     `${mode === 'dark' && 'border-white text-white bg-none'}`,
     'lg:text-[1.5rem] lg:py-[0.3125rem] lg:px-[0.9375rem]',
     'md:text-[0.75rem] md:py-[0.16669rem] md:px-[0.5rem]',
