ee4a8eef53b36198510d692524a1dbda67411d8b
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-26 22:29:41 +0900

    feat: 공통 컴포넌트 Tag 구현 (#138)

---
+++ src/shared/components/Tag.tsx
@@ -0,0 +1,36 @@
+import type React from 'react';
+
+interface TagProps {
+  children: React.ReactNode;
+  mode?: 'blue' | 'dark' | 'light';
+}
+
+const Tag = ({ children, mode = 'blue' }: TagProps) => {
+  const baseClasses = 'inline-flex justify-center items-center rounded-[1.875rem] leading-[140%] font-semibold border';
+
+  const blueClasses = [
+    'border-blue text-blue bg-white',
+    'lg:text-[1rem] lg:py-[0.3125rem] lg:px-[0.9375rem]',
+    'md:text-[0.625rem] md:py-[0.16669rem] md:px-[0.5rem]',
+    'text-[0.4375rem] py-[0.11856rem] px-[0.35563rem]',
+  ].join(' ');
+
+  const lightDarkModeClasses = [
+    `${mode === 'light' && 'border-navyblack text-navyblack bg-white'}`,
+    `${mode === 'dark' && 'border-white text-white bg-none'}`,
+    'lg:text-[1.5rem] lg:py-[0.3125rem] lg:px-[0.9375rem]',
+    'md:text-[0.75rem] md:py-[0.16669rem] md:px-[0.5rem]',
+    'text-[0.75rem] py-[0.1875rem] px-[0.5rem]',
+  ].join(' ');
+
+  return (
+    <>
+      {mode === 'blue' && <div className={`${baseClasses} ${blueClasses}`}>{children}</div>}
+      {(mode === 'dark' || mode === 'light') && (
+        <div className={`${baseClasses} ${lightDarkModeClasses}`}>{children}</div>
+      )}
+    </>
+  );
+};
+
+export default Tag;
