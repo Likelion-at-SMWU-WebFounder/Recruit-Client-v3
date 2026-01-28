c49f644b3c907bd521ead0d5cc939d977dbbd6b4
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-01-28 22:50:31 +0900

    add: subtitle 상수화 추가 (#148)

--- src/pages/activity/constants/index.ts
+++ src/pages/activity/constants/index.ts
@@ -3,3 +3,17 @@ export const TITLE = {
   TITLE_1: '멋대로 도전할 숙명',
   TITLE_2: '정기 세미나부터 실전 프로젝트 협업까지,\n배우고 부딪히며 나만의 성장을 완성합니다.',
 };
+
+// activity 페이지 subtitle
+export const SUB_TITLE = {
+  SUB_TITLE_1: 'what we do',
+  SUB_DESCRIPTION_1: '숙명여대 멋사에서 함께 배우고 성장해요',
+  SUB_TITLE_2: 'annual schedule',
+  SUB_DESCRIPTION_2: '도전과 배움으로 가득 채워진 시간',
+  SUB_TITLE_3: 'curriculum',
+  SUB_DESCRIPTION_3: '기초부터 실전까지 체계적으로 학습해요',
+  SUB_TITLE_4: 'retrospect',
+  SUB_DESCRIPTION_4_PM_PD: '기획・디자인\n운영진 회고',
+  SUB_DESCRIPTION_4_FRONT: '프론트엔드\n운영진 회고',
+  SUB_DESCRIPTION_4_BACK: '백엔드\n운영진 회고',
+};
