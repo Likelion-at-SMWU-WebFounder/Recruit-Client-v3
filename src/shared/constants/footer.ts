f42ffdf725ad49e18720aba18878d1a87f36b2d5
You Dongeun <130419669+uehddms@users.noreply.github.com>
2026-02-05 22:29:45 +0900

    chore: footer 데이터 상수화 변경 (저작권 2026년으로최신화) (#178)

--- src/shared/constants/footer.ts
+++ src/shared/constants/footer.ts
@@ -1,4 +1,4 @@
 export const Footer = {
   TITLE: 'LIKELION \n AT SOOKMYUNG',
-  COPYRIGHT: 'Copyrightⓒ2025 숙명여자대학교 멋쟁이사자처럼 All rights reserved.',
+  COPYRIGHT: 'Copyrightⓒ2026 숙명여자대학교 멋쟁이사자처럼 All rights reserved.',
 };
